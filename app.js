9ff79afbeda3a7447d986322fd8de08bc5334b45
Jeremy Parker <jeremyeparker@yahoo.com>
2020-08-26 15:32:44 -0400

    require dotenv, add mongo.connect, app.listen wip

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+//requirements
+require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
-
+//express app
 const app = express();
+//dotevn requirements
+const port = process.env.PORT || 3005;
+const connectURI = process.env.MONGO;
 
+mongoose.connect();
+
+app.listen(port);
