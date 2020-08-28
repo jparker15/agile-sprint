192c2cfa8db479bee9c14ba17e1b7c2113f8f1df
Jeremy Parker <jeremyeparker@yahoo.com>
2020-08-27 22:09:49 -0400

    add express.static, req/use homeRouter, start server listener

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-//requirements
 require("dotenv").config();
+
+//packages
 const mongoose = require("mongoose");
 const express = require("express");
-//express app
-const app = express();
-//dotevn requirements
+
+//dotevn consts 
 const port = process.env.PORT || 3005;
 const connectURI = process.env.MONGO;
 
-mongoose.connect();
+//main express app
+const app = express();
 
-app.listen(port);
+app.use(express.static("static"));
+
+const homeRouter = require("./routes/homeRouter");
+
+app.use("/", homeRouter);
+
+app.get("/", (res,req) =>{
+    res.json({"message":"message world"});
+});
+
+// mongoose.connect(connectURI);
+//start server liserner
+app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}`);
+
+});
