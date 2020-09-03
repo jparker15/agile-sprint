e870e2435803fcf23e1410adf2849e9765b43f93
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-03 18:52:53 -0400

    create router for user related routes

---
+++ routes/userRouter.js
@@ -0,0 +1,10 @@
+//model
+const User = require("../models/User");
+//dependents
+const router = require("express").Router();
+
+router.get("/",(req,res) =>{
+    res.send("user router")
+})
+
+module.exports = router;
